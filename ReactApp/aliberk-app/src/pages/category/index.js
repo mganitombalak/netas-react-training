248a8cd8d7ed8871839b3b217c2a3e85d5501a23
mganitombalak <mgani.tombalak@yahoo.com>
2020-03-04 11:59:48 +0300

    Axios

--- ReactApp/aliberk-app/src/pages/category/index.js
+++ ReactApp/aliberk-app/src/pages/category/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import axios from 'axios';
+class Category extends React.Component {
+    state = { categories: null }
+    componentDidMount() {
+        axios.get('http://178.128.248.160:81/api/category', {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjJjYTc5NjVhLTk3NWQtNGNiOC05OGNjLTIzZDg5Y2M5YzU3YiIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1ODMzMTExOTEsImV4cCI6MTU4MzMxMjA5MSwiaWF0IjoxNTgzMzExMTkxfQ.VsDr_XEf_EvOl8eMm0DdrtXo7xviYKmhLYPFvBXXkjQ'
+            }
+        })
+            .then(res => this.setState({categories:res.data}))
+            .catch(e => console.log(e));
+    }
 
-const Category = () => {
-    return <div>CATEGORY PAGE!</div>;
+    render() { return <div>CATEGORY PAGE!</div>; }
 };
 export default Category;
